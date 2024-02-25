4fe5de6c2d23012637ce328fe23d7f5e8400a99c
VanessaRupertus <100003877+VanessaRupertus@users.noreply.github.com>
2024-02-25 14:00:22 -0500

    Playing Around with Flask
    tweaking html, js, and python script to see what works with Flask and what doesn't.

--- app.py
+++ app.py
@@ -5,13 +5,33 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    """ Displays the index page accessible at '/' """
     return render_template('index.html')
 
 
 @app.route('/result/', methods=['POST'])
 def result():
-    return render_template, 'index.html'
+    result_processed = 0
+    try:
+
+        equation = request.form['result']
+        if "+" in equation:
+            arr = equation.split("+", 1)
+            result_processed = float(arr[0]) + float(arr[1])
+        elif "-" in equation:
+            arr = equation.split("-", 1)
+            result_processed = float(arr[0]) - float(arr[1])
+        elif "*" in equation:
+            arr = equation.split("*", 1)
+            result_processed = float(arr[0]) * float(arr[1])
+        elif "/" in equation:
+            arr = equation.split("/", 1)
+            result_processed = float(arr[0]) / float(arr[1])
+
+        return render_template('index.html', result=result_processed, success=True)
+    except ZeroDivisionError:
+        return render_template('index.html', result="Error", error="You cannot divide by zero.", success=False)
+    except ValueError:
+        return render_template('index.html', result="Error", error="Equation entered not valid.", success=False)
 
 
 if __name__ == '__main__':
--- static/calc.js
+++ static/calc.js
@@ -1,7 +1,44 @@
-function clearScreen() {
+function clear() {
     document.getElementById("result").value = "";
+    enable_btns();
 }
 
 function display(value) {
     document.getElementById("result").value += value;
+}
+
+function disable_btns() {
+    document.getElementById("0").disabled = true;
+    document.getElementById("1").disabled = true;
+    document.getElementById("2").disabled = true;
+    document.getElementById("3").disabled = true;
+    document.getElementById("4").disabled = true;
+    document.getElementById("5").disabled = true;
+    document.getElementById("6").disabled = true;
+    document.getElementById("7").disabled = true;
+    document.getElementById("8").disabled = true;
+    document.getElementById("9").disabled = true;
+    document.getElementById("+").disabled = true;
+    document.getElementById("-").disabled = true;
+    document.getElementById("*").disabled = true;
+    document.getElementById("/").disabled = true;
+    document.getElementById(".").disabled = true;
+}
+
+function enable_btns() {
+    document.getElementById("0").disabled = false;
+    document.getElementById("1").disabled = false;
+    document.getElementById("2").disabled = false;
+    document.getElementById("3").disabled = false;
+    document.getElementById("4").disabled = false;
+    document.getElementById("5").disabled = false;
+    document.getElementById("6").disabled = false;
+    document.getElementById("7").disabled = false;
+    document.getElementById("8").disabled = false;
+    document.getElementById("9").disabled = false;
+    document.getElementById("+").disabled = false;
+    document.getElementById("-").disabled = false;
+    document.getElementById("*").disabled = false;
+    document.getElementById("/").disabled = false;
+    document.getElementById(".").disabled = false;
 }
